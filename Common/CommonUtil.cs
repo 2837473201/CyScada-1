990f518f5b3966162751a730ae94da036e971134
xieyuliang <xieyuliang1985@126.com>
2016-07-07 13:19:41 +0800

    ShiftToDirectOPC

---
+++ Common/CommonUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Threading;
+
+namespace CyScada.Web.Common
+{
+    public static  class CommonUtil
+    {
+       
+
+    }
+}
--- CyScada.Common/CommonUtil.cs
+++ CyScada.Common/CommonUtil.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 
 namespace CyScada.Common
 {
@@ -9,6 +12,11 @@ namespace CyScada.Common
     {
         public const string DefaultUserPassword="118DD9CC92B899DF";//123456加密
         public const string AuthSeperator = ",";
+
+
+        private static Queue<string> logQueue = new Queue<string>();
+        private static object locker = new object();
+        private static bool runner = true;
 
         public enum MappingType
         {
@@ -75,5 +83,43 @@ namespace CyScada.Common
 
             return currentAuth.Contains(authCode);
         }
+
+
+
+        public static void EnqueueLog(string strMessage)
+        {
+            lock (locker)
+            {
+                logQueue.Enqueue(strMessage);
+            }
+        }
+
+        public CommonUtil()
+        {
+            var currentPath = Path.GetFullPath(Assembly.GetExecutingAssembly().Location);
+            ThreadPool.QueueUserWorkItem(w =>
+            {
+                while (runner)
+                {
+                    if (logQueue.Any())
+                    {
+                        var message = string.Empty;
+                        lock (locker)
+                        {
+                            message = logQueue.Dequeue();
+                        }
+                        File.AppendAllText(currentPath + "\\Log\\log.txt", "【" +
+                                                                           DateTime.Now.ToString(
+                                                                               "yyyy-MM-dd HH:mm:ss:fff") + "】" +
+                                                                           message);
+                    }
+                    Thread.Sleep(100);
+                }
+            });
+        }
+
+
+
+
     }
 }
